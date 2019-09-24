741acac6230c0693a3493e4edabc5080966fee3f
Gabriel Lima <gabriellfsouza@gmail.com>
2019-09-24 07:50:55 -0300

    chore: next button handering and loading component.

--- web/src/components/UserList/index.js
+++ web/src/components/UserList/index.js
@@ -1,31 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { MdNavigateNext } from 'react-icons/md';
+import ReactLoading from 'react-loading';
+
 import { Container } from './styles';
 
 import api from '~/services/api';
 
 export default function UserList() {
+  const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
   const [nextEndpoint, setNextEndpoint] = useState('/api/users');
 
   async function getNextUsers() {
+    setLoading(true);
     try {
       const response = await api.get(nextEndpoint);
-    
+
       setNextEndpoint(response.headers['relative-link-url']);
       setUsers([...users, ...response.data]);
     } catch (error) {
-      console.log(error);
-      alert(error);
+      alert('Falha ao tentar carregar.');
     }
-    
+    setLoading(false);
   }
 
   useEffect(() => {
     getNextUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
+  return loading === true ? (
+    <ReactLoading type="bars" color="black" />
+  ) : (
     <Container>
       <ul>
         {users.map(user => (
--- web/src/components/UserList/styles.js
+++ web/src/components/UserList/styles.js
@@ -1,15 +1,34 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.div`
+  margin-top: 30px;
   display: flex;
   flex-direction: row;
-  align-items: flex-start;
-  justify-content: space-between;
 
   ul {
   }
 
   button {
-    background: none;
+    background: #7159c1;
+    border: 0;
+    padding: 0 15px;
+    margin-left: 10px;
+    padding: 10px;
+    border-radius: 4px;
+    align-self: flex-start;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.05, '#7159c1')};
+    }
+
+    &:active {
+      background: ${darken(0.15, '#7159c1')};
+    }
   }
 `;
